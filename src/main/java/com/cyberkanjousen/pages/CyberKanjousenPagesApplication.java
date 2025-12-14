7a69812a298e21751d013e1bde495b74a1499ec7
CyberKanjousen <d_w_b@outlook,com>
2025-12-14 19:35:37 +0800

    1

---
+++ src/main/java/com/cyberkanjousen/pages/CyberKanjousenPagesApplication.java
@@ -0,0 +1,13 @@
+package com.cyberkanjousen.pages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberKanjousenPagesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CyberKanjousenPagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cyberkanjousen/pages/CyberKanjousenPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberkanjousen.pages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberKanjousenPagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
